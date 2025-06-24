9833ad0bd9dd3df52480e1d2e870a252be661e85
Mason <mason@hicksm.dev>
2025-06-24 11:27:49 -0400

    Updated auth0 to use custom domain

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -8,13 +7,11 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <Auth0Provider
-        domain="dev-naj38ze0hqye82m0.us.auth0.com"
-        clientId="QDNZ39OyUvcuAvS9HqdH0iywVUN6aNGH">
-      <App />
-    </Auth0Provider>
-  </BrowserRouter>
+  <Auth0Provider
+      domain="auth0.portfolio.dev.aidsbooger.com"
+      clientId="QDNZ39OyUvcuAvS9HqdH0iywVUN6aNGH">
+    <App />
+  </Auth0Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
