9626f1e9296ee2d4afa22ad80fb8bbe1bf87611b
Mason <mason@hicksm.dev>
2025-06-24 10:59:36 -0400

    Included BrowserRouter, allowing Auth0 to cache token appropriately

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
@@ -7,12 +8,13 @@ import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Auth0Provider
-      domain="dev-naj38ze0hqye82m0.us.auth0.com"
-      clientId="QDNZ39OyUvcuAvS9HqdH0iywVUN6aNGH"
-      cacheLocation='memory'>
-    <App />
-  </Auth0Provider>
+  <BrowserRouter>
+    <Auth0Provider
+        domain="dev-naj38ze0hqye82m0.us.auth0.com"
+        clientId="QDNZ39OyUvcuAvS9HqdH0iywVUN6aNGH">
+      <App />
+    </Auth0Provider>
+  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
