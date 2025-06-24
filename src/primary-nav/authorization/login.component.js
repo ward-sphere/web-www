bc7d818f0284db17d935135be6c8a98838fac6d8
Mason <mason@hicksm.dev>
2025-06-24 10:03:14 -0400

    Set redirect uri for login/logout

--- src/primary-nav/authorization/login.component.js
+++ src/primary-nav/authorization/login.component.js
@@ -4,7 +4,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Login = () => {
   const { loginWithRedirect } = useAuth0();
 
-  return <img src="/logo48.png" alt="logo"  onClick={() => loginWithRedirect()}/>;
+  return <img src="/logo48.png" alt="logo"  onClick={() => loginWithRedirect({
+    authorizationParams: {
+      redirect_uri: window.location.origin
+    }
+  })}/>;
 };
 
 export default Login;
--- src/primary-nav/authorization/logout.component.js
+++ src/primary-nav/authorization/logout.component.js
@@ -4,7 +4,11 @@ import { useAuth0 } from "@auth0/auth0-react";
 const Logout = () => {
   const { logout } = useAuth0();
 
-  return <img src="/logo48.png" alt="logo"  onClick={() => logout()}/>;
+  return <img src="/logo48.png" alt="logo"  onClick={() => logout({
+    logoutParams: {
+      returnTo: window.location.origin
+    }
+  })}/>;
 };
 
 export default Logout;
